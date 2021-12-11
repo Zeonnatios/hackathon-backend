c62900996e971e538f18a881fd698e8468b4968b
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-11 20:45:32 -0300

    feat: adicionado variaveis de ambiente

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
