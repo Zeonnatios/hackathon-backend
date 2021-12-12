abe03bb1bf72c07c4d6f4ca313a940b2758ac4aa
Rodrigo <rodrigo.facury14@gmail.com>
2021-12-12 17:27:07 -0300

    feat: logica de adicionar trilha no array minhas trilhas do user

--- src/services/trailServices.js
+++ src/services/trailServices.js
@@ -1,8 +1,10 @@
 const trailModel = require('../models/trailModel');
+const userModel = require('../models/userModel');
 
 const createNewTrail = async (trail) => {
   const newTrail = { ...trail, likes: 0 };
   const data = await trailModel.createNewTrail(newTrail);
+  await userModel.updateUserByName(trail);
   return data;
 };
 
