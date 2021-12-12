57494b025bf355f35fb0ace672c1fc4b87b04ddf
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-11 21:34:29 -0300

    feat: removendo retorno do password - getUsers

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -2,6 +2,7 @@ const userModel = require('../models/userModel');
 
 const createNewuser = async (user) => {
   const data = await userModel.createNewUser(user);
+  delete data.password;
   return data;
 };
 
