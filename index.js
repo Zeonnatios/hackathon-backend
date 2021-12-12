60f6934a5c86e10392520b02cf7443854d5730aa
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-11 21:23:54 -0300

    atualizando importaçoes na index

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const { usersRoute } = require('./src/routes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/', [usersRoute]);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
