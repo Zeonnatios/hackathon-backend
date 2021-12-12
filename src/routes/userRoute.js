d954bd4241b0441e9f1ba3408cbcf007150e3e39
Matheus Antonio <matheusantonio232@gmail.com>
2021-12-12 13:05:49 -0300

    feat: adicionado o put na rota de usuario

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { createNewuser } = require('../controllers/userController');
+const { createNewuser, updateUser } = require('../controllers/userController');
 const { registerAuthenticator, loginAuthenticator, createToken } = require('../middleware');
 
 router.post('/users/', [registerAuthenticator, createNewuser]);
 router.post('/login/', [loginAuthenticator, createToken]);
+
+router.put('/users/:id', updateUser);
 
 module.exports = router;
